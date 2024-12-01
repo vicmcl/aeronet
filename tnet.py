d07f59d7051c8d9cdcb3fe0df83c565208b3f535
vicmcl <victorienmichel3@gmail.com>
2024-12-01 09:16:39 +0100

    FEAT: implement coordinates and features transformations

---
+++ tnet.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TNet(nn.Module):
+    def __init__(self, k):
+        super().__init__()
+        self.k = k
+
+        self.conv1 = nn.Conv1d(k, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 1024, 1)
+
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k * k)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        ident = torch.eye(self.k, dtype=torch.float32, requires_grad=True)
+        ident = ident.view(1, self.k * self.k).repeat(batch_size, 1)
+        if x.is_cuda:
+            ident = ident.cuda()
+
+        x = x + ident
+        x = x.view(-1, self.k, self.k)
+        return x
